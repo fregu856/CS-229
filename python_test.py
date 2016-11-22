121ba78af2cf6fb4029486823748dbc8a748e001
Fredrik Gustafsson <fregu856@student.liu.se>
2016-11-21 19:46:11 -0800

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -1,12 +1,14 @@
 import gym
+
 env = gym.make("CartPole-v0")
 for i_episode in range(20):
     observation = env.reset()
     for t in range(10000):
         env.render()
         #print(observation)
-        action = env.action_space.sample()
+        #action = env.action_space.sample()
         #print(action)
+        action = [0]
         observation, reward, done, info = env.step(action)
         if done:
             print("Episode finished after " + str(t+1) + " timesteps")
